4426012fa30ca315218f46be9823d5a6506857c3
jsump <libbyn79@gmail.com>
2024-03-21 21:54:24 +0000

    Create stored procedure that adds a new correction

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,13 +1,7 @@
 -- List all bands with Glam rock as their main style
 -- ranked by longetivity
 SELECT band_name,
-	IFNULL(
-		CASE
-			WHEN split IS NOT NULL THEN split - formed
-			ELSE YEAR(NOW()) - formed
-		END,
-		0
-	) AS lifespan
+	DATEDIFF(2022, IFNULL(split, 2022)) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,15 @@
+-- Create a stored procedure that adds a new correction for a student
+CREATE PROCEDURE AddBonus (IN p_user_id INT, IN p_project_name VARCHAR(255), IN p_score INT)
+BEGIN
+	DECLARE project_id INT;
+
+	SELECT id INTO project_id FROM projects WHERE name = p_project_name;
+	IF project_id IS NULL THEN
+		INSERT INTO projects (name) VALUES (p_project_name);
+		SET project_id = LAST_INSEART_ID();
+	END IF;
+
+	INSERT INTO corrections (user_id, project_id, score) VALUES (p_user_id, project_id, p_score);
+END;
+//
+DELIMITER ;
