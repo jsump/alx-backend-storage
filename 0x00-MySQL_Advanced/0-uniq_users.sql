952dbe62b44efbeb49cd761d7ceeedda07208ced
jsump <libbyn79@gmail.com>
2024-03-21 12:01:07 +0000

    Task 0 edit

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Create a table 'users' with id, email and string
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTOINCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
