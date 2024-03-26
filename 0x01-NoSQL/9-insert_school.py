d0ce83337feac1573e9a394939b2610441b89fdb
jsump <libbyn79@gmail.com>
2024-03-26 06:50:27 -0700

    Insert a new doc in a collection based on kwargs

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,20 @@
+#!/usr/bin/en python3
+"""
+Module: 9-insert_school.py
+
+This module contains a function that inserts a new document
+in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    This function insers a new document in a collection
+    based on kwargs.
+
+    mongo_collection will be pymongo collection object
+
+    Returns the new _id
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
