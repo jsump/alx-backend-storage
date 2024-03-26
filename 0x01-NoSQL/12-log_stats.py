26fc7c4f3b4ad582b0b31452692a94952da46ba7
jsump <libbyn79@gmail.com>
2024-03-26 08:18:49 -0700

    Edit methods

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -25,11 +25,12 @@ def display_nginx_stats():
     total_logs = collection.count_documents({})
 
     print(f"{total_logs} logs")
+    print(f"Methods:")
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        print(f" \tmethod {method}: {count}")
 
     status_count = collection.count_documents(
             {"method": "GET", "path": "/status"}
