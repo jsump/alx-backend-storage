87fca23fcc0dadfe4ebc760f2278f7a233cc4ab1
jsump <libbyn79@gmail.com>
2024-03-26 07:59:44 -0700

    Nginx logs

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Module: 12-log_stats.py
+
+This module contains a fucntion that provides some stats abount Ngix
+stored in MongoDB
+"""
+
+
+from pymongo import MongoClient
+
+
+def display_nginx_stats():
+    """
+    This function provides stats on Ngix logs stores
+    in MongoDB
+
+    Database: logs
+    Collection: nginx
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    database = client.logs
+    collection = database.nginx
+
+    total_logs = collection.count_documents({})
+
+    print(f"{total_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_count = collection.count_documents(
+            {"method": "GET", "path": "/status"}
+            )
+    print(f"{status_count} status check")
+
+
+if __name__ == "__main__":
+    display_nginx_stats()
