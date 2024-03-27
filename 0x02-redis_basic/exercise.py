82254cbae590beb9c3090b94426d4d5f01378191
jsump <libbyn79@gmail.com>
2024-03-27 11:41:20 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,7 +9,7 @@ This module contains a class Cache.
 import uuid
 import redis
 import pickle
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -40,3 +40,38 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """
+        This method takes a key string argument and an optiional
+        Callable argument named fn.
+        """
+        value = self._redis.get(key)
+        if value is not None and fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key):
+        """
+        This callable function decodes the value as a UTF-8 string
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key):
+        """
+        This function converts the value to an integer
+        """
+        return self.get(key, fn=int)
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
